8c55512a25f97467892455284bb1575b2c6be566
summer506hai <1170618497@qq.com>
2024-02-11 21:51:17 +0800

    Create 猜字谜.py
    提交代码

---
+++ leetcode/猜字谜.py
@@ -0,0 +1,41 @@
+# -*— coding:utf-8 --*
+'''
+输入谜面和谜底
+谜面单词列表，以 , 分割
+谜底单词列表，以 , 分割
+输出： 匹配到 正确单词列表，以 , 分割，匹配不到 返回  not found
+1、变换顺序后 一样 2、字母去重后一样 —— 算匹配上
+'''
+
+def remove_duplicates(question, answer):
+    q = list(set(question))
+    a = list(set(answer))
+    if q == a:
+        return True
+    return False
+
+def change(question,answer):
+    q = sorted(list(set(question)))
+    a = sorted(list(set(answer)))
+    if q == a:
+        return True
+    return False
+
+def guess_game():
+    result_list = []
+    questions = input().split(",")
+    answers = input().split(",")
+    for q in questions:
+        flag = False
+        for a in answers:
+            if remove_duplicates(q,a) or change(q,a):
+                result_list.append(a)
+                flag = True
+                break
+        if not flag:
+            result_list.append("not found")
+
+    return ",".join(result_list)
+
+if __name__ == "__main__":
+    print(guess_game())
